ee615b036533bea6ae6ae6a7e975824ea852c1c7
Lyka0n <johtoalex@gmail.com>
2024-03-25 16:48:52 +0100

    login updated

--- SocialNetworkFE/src/app/login/login.component.ts
+++ SocialNetworkFE/src/app/login/login.component.ts
@@ -6,31 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  register() {
-    const formBox = document.querySelector('.form-box');
-    if (formBox) {
-      formBox.classList.remove('register-active');
-      formBox.classList.add('login-active');
-
-      const forms = document.querySelectorAll('.input-group');
-      forms.forEach(form => {
-        form.classList.remove('form-right'); // Rimuovi la classe form-right
-        form.classList.add('form-left'); // Aggiungi la classe form-left
-      });
-    }
-  }
 
   login() {
-    const formBox = document.querySelector('.form-box');
-    if (formBox) {
-      formBox.classList.remove('login-active'); // Rimuovi questa classe se stai passando alla registrazione
-      formBox.classList.add('register-active'); // Aggiungi questa classe per mostrare il form di registrazione
-
-      const forms = document.querySelectorAll('.input-group');
-      forms.forEach(form => {
-        form.classList.remove('form-left'); // Rimuovi la classe form-left
-        form.classList.add('form-right'); // Aggiungi la classe form-right
-      });
+    const formBox = document.querySelector('.input-group');
+    const btn = document.getElementById('btn');
+    if (formBox && btn) {
+        formBox.classList.add('form-right');
+        formBox.classList.remove('form-left');
+        btn.classList.add('button-right');
+        btn.classList.remove('button-left');
     }
+}
+
+  register() {
+    const formBox = document.querySelector('.input-group');
+    const btn = document.getElementById('btn');
+    if (formBox && btn) {
+        formBox.classList.add('form-left');
+        formBox.classList.remove('form-right');
+        btn.classList.add('button-left');
+        btn.classList.remove('button-right');
+    }
+}
+
+
+
+  submitLogin() {
+    // Handle login form submission logic here
   }
+
+  submitRegister() {
+    // Handle registration form submission logic here
+  }
+
 }
